56b8d7db16e73d73f5566ad3a7dac7e1f74ac6f7
Isidro Moreno <imoreno.itservices@gmail.com>
2022-09-24 00:03:49 -0300

    [chore] Jest - Replace Jasmine with Jest

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const jestConfig: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^environments/(.*)$': '<rootDir>/src/environments/$1'
+  },
+  collectCoverage: true
+};
+
+export default jestConfig;
