c7cd8f7f9585de703a66de1d0a238085c895ac7b
Isidro Moreno <imoreno.itservices@gmail.com>
2022-09-27 02:35:32 -0300

    [test] (WIP) ErrorHandlerService unit tests. LoginListComponent unit tests updates

--- src/app/components/login-list/login-list.component.spec.ts
+++ src/app/components/login-list/login-list.component.spec.ts
@@ -1,17 +1,17 @@
 import { HttpClient } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { NGZORRO_MODULES } from 'src/app/ng-zorro';
 
-import { COMPONENTS } from '..';
-import { LoginListComponent } from './login-list.component';
+import { BehaviorSubject, of } from 'rxjs';
 import { GithubService } from 'src/app/services/github.service';
-import { SearchResultsPaginationData, User } from '../../model';
-import { of, BehaviorSubject } from 'rxjs';
-import { searchResultsPaginationDataMock } from '../../model/testing/model-mocks';
+import { COMPONENTS } from '..';
+import { LinksRelation } from '../../model/search-results-page';
 import { linkFirst } from '../../model/testing/link-mocks';
+import { LoginListComponent } from './login-list.component';
+import { searchResultsPaginationDataMock, searchResultsPaginationDataMock2 } from 'src/app/model/testing/model-mocks';
 
 describe('LoginListComponent', () => {
   let component: LoginListComponent;
@@ -28,7 +28,9 @@ describe('LoginListComponent', () => {
           provide: GithubService,
           useValue: {
             findUserInLogin: () => of(searchResultsPaginationDataMock),
-            currentPageSubject: new BehaviorSubject<number>(1)
+            fetchLinkUri: () => of(searchResultsPaginationDataMock2),
+            currentPageSubject: new BehaviorSubject<number>(1),
+            isLoadingSubject: new BehaviorSubject<boolean>(false)
           }
         }
       ]
@@ -47,15 +49,49 @@ describe('LoginListComponent', () => {
     expect(component.paginationLinks).toBeNull();
     expect(component.paginationEnabled).toBe(false);
     expect(component.totalPages).toBe(0);
+    expect(component.isNewSearch).toBe(false);
+    expect(component.isFirstSession).toBe(true);
   });
 
-  it('should update paginated result list properties binding after "searchLogin"', () => {
-    expect(component.userResultList).toBeNull();
-    expect(component.paginationEnabled).toBe(false);
+  describe('#searchLogin', () => {
+    it('should update paginated result list properties binding after "searchLogin" call', () => {
+      expect(component.userResultList).toBeNull();
+      expect(component.paginationLinks).toBeNull();
+      expect(component.paginationEnabled).toBe(false);
+      expect(component.totalPages).toBe(0);
+      expect(component.isNewSearch).toBe(false);
+      expect(component.isFirstSession).toBe(true);
 
-    component.searchLogin('test');
-    expect(component.userResultList).toBe(searchResultsPaginationDataMock.searchResults?.items);
-    expect(component.paginationLinks?.has('uri', linkFirst));
+      component.searchLogin('test');
+      fixture.detectChanges();
+      expect(component.userResultList).toBe(searchResultsPaginationDataMock.searchResults?.items);
+      expect(component.paginationLinks?.has('uri', linkFirst));
+      expect(component.paginationEnabled).toBe(true);
+      expect(component.totalPages).toBe(112);
+      expect(component.isNewSearch).toBe(false);
+      expect(component.isFirstSession).toBe(false);
+    });
+  });
+
+  describe('#goToLink', () => {
+    it('should update paginated result list properties binding after "goToLink" call', () => {
+      component.searchLogin('test');
+      fixture.detectChanges();
+      expect(component.userResultList).toBe(searchResultsPaginationDataMock.searchResults?.items);
+      expect(component.paginationLinks?.has('uri', linkFirst));
+      expect(component.paginationEnabled).toBe(true);
+      expect(component.totalPages).toBe(112);
+      expect(component.isNewSearch).toBe(false);
+      expect(component.isFirstSession).toBe(false);
+      component.goToLink(LinksRelation.NEXT);
+      fixture.detectChanges();
+      expect(component.paginationEnabled).toBe(true);
+      expect(component.paginationLinks?.has('uri', linkFirst));
+      expect(component.userResultList).toBe(searchResultsPaginationDataMock2.searchResults?.items);
+      expect(component.totalPages).toBe(112);
+      expect(component.isNewSearch).toBe(false);
+      expect(component.isFirstSession).toBe(false);
+    });
   });
 
   it('should create', () => {
--- src/app/services/error-handler.service.spec.ts
+++ src/app/services/error-handler.service.spec.ts
@@ -1,16 +1,67 @@
-import { TestBed } from '@angular/core/testing';
-
+import { Location } from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from '../app.component';
+import { AppModule } from '../app.module';
+import { UnauthorizedErrorComponent } from '../components/errors/unauthorized-error/unauthorized-error.component';
+import { routes } from '../routes';
 import { ErrorHandlerService } from './error-handler.service';
 
 describe('ErrorHandlerService', () => {
-  let service: ErrorHandlerService;
+  let errorHandlerService: ErrorHandlerService;
+  let location: Location;
+  let router: Router;
+  let fixture: AppComponent;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ErrorHandlerService);
+    TestBed.configureTestingModule({
+      imports: [AppModule, RouterTestingModule.withRoutes(routes)],
+      declarations: [AppComponent, UnauthorizedErrorComponent],
+      providers: [ErrorHandlerService]
+    }).compileComponents();
+    errorHandlerService = TestBed.inject(ErrorHandlerService);
+    location = TestBed.inject(Location);
+    router = TestBed.inject(Router);
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
+  });
+
+  describe('#handleError', () => {
+    it('should redirect to /error/unauthorized', fakeAsync(() => {
+      const httpResponse = new HttpErrorResponse({ status: 401 });
+      errorHandlerService.handleError(httpResponse);
+      tick();
+      expect(location.path()).toEqual('/error/unauthorized');
+    }));
+    it('should redirect to /error/rate-exceeded', fakeAsync(() => {
+      const httpResponse = new HttpErrorResponse({ status: 403 });
+      errorHandlerService.handleError(httpResponse);
+      tick();
+      expect(location.path()).toEqual('/error/rate-exceeded');
+    }));
+    it('should redirect to /error/server', fakeAsync(() => {
+      const httpResponse = new HttpErrorResponse({ status: 503 });
+      errorHandlerService.handleError(httpResponse);
+      tick();
+      expect(location.path()).toEqual('/error/server');
+    }));
+    it('should redirect to /error/server', fakeAsync(() => {
+      const httpResponse = new HttpErrorResponse({ status: 500 });
+      errorHandlerService.handleError(httpResponse);
+      tick();
+      expect(location.path()).toEqual('/error/server');
+    }));
+    it('should redirect to /error/offline', fakeAsync(() => {
+      const httpResponse = new HttpErrorResponse({ status: 0 });
+      errorHandlerService.handleError(httpResponse);
+      tick();
+      expect(location.path()).toEqual('/error/offline');
+    }));
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(errorHandlerService).toBeTruthy();
   });
 });
